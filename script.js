91746cc9fda7b917a6e584ce3b3d941e70552c6e
Gustavo Ale <gustavo.alexandredj@gmail.com>
2024-06-11 20:04:03 -0300

    Clear code js

--- script.js
+++ script.js
@@ -7,26 +7,18 @@ function toggleMode() {
 
   if (html.classList.contains("Bat")) {
     img.setAttribute("src", "./Assets/Avatar/avatar-bat.png")
-  } else {
-    img.setAttribute("src", "./Assets/Avatar/avatar-super.png")
-  }
-
-  if (html.classList.contains("Bat")) {
     img.setAttribute("alt", "Gustavo sorrindo, camisa preta e fundo cinza")
   } else {
+    img.setAttribute("src", "./Assets/Avatar/avatar-super.png")
     img.setAttribute("alt","Gustavo sorrindo usando óculos, blusa azul com fundo azul")
   }
 
   img = document.querySelector("#marca img")
   if (html.classList.contains("Bat")) {
     img.setAttribute("src", "./Assets/Marca/Bat-marca.png")
-  } else {
-    img.setAttribute("src", "./Assets/Marca/Super-marca.png")
-  }
-
-  if (html.classList.contains("Bat")) {
     img.setAttribute("alt", "Logo escrito Batman")
   } else {
+    img.setAttribute("src", "./Assets/Marca/Super-marca.png")
     img.setAttribute("alt", "Logo escrito Superman em vermelho e amarelo")
   }
 }
